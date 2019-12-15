ee880eba4867ce8b5fe88a6ac28c320ad9a09676
Tatsuya Sato <tatsuya.g.sato@gmail.com>
2019-12-16 02:30:21 +0900

    tite

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
+import React from "react";
+import { Container, Row, Col, Button } from "reactstrap";
 
-const renderTable = (data) => {
-   const mapped = data.map(i => {
+const Cards = ({ data }) => {
+  const renderTable = data => {
+    const mapped = data.map(i => {
+      return (
         <Row className="row1" xs="10">
-        <Col>{i.name}</Col>
-        <Col>{i.brand}</Col>
-        <Col>{i.packagingAmount}</Col>
-        <Col>{i.packaging}</Col>
-        <Col>{i.stock}</Col>
-        <Col>{i.threshold}</Col>
-        <Col>{i.ceiling}</Col>
-        <Col>{i.unit}</Col>
-        <Col>{i.amount}</Col>
-        <Col>{i.status}</Col>
-        <Col><Button color="primary" size="sm">Edit</Button></Col>
-        <Col><Button color="secondary" size="sm">Delete</Button></Col>
-      </Row>
-    })
-    console.log(mapped)
-}
+          <Col>{i.name}</Col>
+          <Col>{i.brand}</Col>
+          <Col>{i.packagingAmount}</Col>
+          <Col>{i.packaging}</Col>
+          <Col>{i.stock}</Col>
+          <Col>{i.threshold}</Col>
+          <Col>{i.ceiling}</Col>
+          <Col>{i.unit}</Col>
+          <Col>{i.amount}</Col>
+          <Col>{i.status}</Col>
+          <Col>
+            <Button color="primary" size="sm">
+              Edit
+            </Button>
+          </Col>
+          <Col>
+            <Button color="secondary" size="sm">
+              Delete
+            </Button>
+          </Col>
+        </Row>
+      );
+    });
+    return mapped;
+  };
 
-const Cards = (props) => {
-  return (
-    <Container>    
-    {
-        renderTable(props.data)
-    }
-    </Container>
-  );
-}
+  return <Container>{renderTable(data)}</Container>;
+};
 
 export default Cards;
